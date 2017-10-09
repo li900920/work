cf6ed45b3e7aa21052cb71a52ef882b2af6acb2b
li900920 <451948174@qq.com>
2017-10-09 19:44:53 +0800

    first commit

---
+++ entry.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Content = React.createClass({
+	render:function(){
+		return(
+			<div>
+			111
+			</div>
+		)
+	}
+})
+
+ReactDOM.render(<Content/>,document.getElementById('box'))
